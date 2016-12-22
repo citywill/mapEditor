1c93b46be37492aa1f35caeba329ef841488cb9d
zhangsz <citywill@gmail.com>
2016-12-22 14:24:13 +0800

    追加区域数据到 json

--- draw-save.php
+++ draw-save.php
@@ -1,5 +1,32 @@
 <?php
-var_dump($_POST);die();
+//var_dump($_POST);die();
 
 $mapId = $_POST['id'];
 $data = $_POST['data'];
+
+foreach ($data as $key => $value) {
+    $data[$key] = array($value['lng'], $value['lat']);
+}
+
+$jsonFile = $jsonPath . '/' . $mapId . '.json';
+
+$jsonData = file_get_contents($jsonFile);
+
+$jsonData = json_decode($jsonData, true);
+
+$jsonData['regins'][] = array(
+
+    'properties' => array(
+        'name' => '新社区',
+        'id' => '23232323',
+    ),
+
+    'geometry' => array(
+        'type' => 'MultiPolygon',
+        'coordinates' => array(array($data)),
+    ),
+);
+
+file_put_contents($jsonFile, json_encode($jsonData));
+
+//var_dump($jsonData);
